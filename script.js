86bee42950bce80a61037c233915b9e25e4fa89b
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-02-14 19:07:19 +0000

    Mobile view

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var hamburgerButton = document.getElementById('hamburger')
+var navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
